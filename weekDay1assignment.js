ec38b456962cd01b1ad1b4473ade1915cc0e3f5e
poonamsyadav <112817586+poonamsyadav@users.noreply.github.com>
2026-03-21 12:51:37 -0600

    Add files via upload
    week1day1 assignment uploaded

---
+++ weekDay1assignment.js
@@ -0,0 +1,16 @@
+//Day1 Assignment
+
+let userName ="First Playwright Assignment"; //String
+console.log(typeof userName);
+
+let organization ="XYZ consultancy"; //String
+console.log(typeof organization);
+
+let contactNumber= 9876543210; //Number
+console.log(typeof contactNumber);
+
+let knowsAutomation = true; //Boolean
+console.log(typeof knowsAutomation);
+
+let usesPlaywright; //undefined
+console.log(typeof usesPlaywright);
